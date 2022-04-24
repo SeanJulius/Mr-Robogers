48752dcfb95f386705c477afe7df1d70a5596d3e
Sean <crimsonsand@live.com>
2022-04-23 20:28:15 -0700

    Add README information

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 //Business Logic
 
+//First test should return "1" with Beep!
 function beep(input) {
   let numberStr = "";
   for(count = 0; count <= input; count++) {
@@ -7,12 +8,15 @@ function beep(input) {
     if (count.includes('3')) {
       numberStr = numberStr.concat(", Won't you be my neighbor?");
     }
+    //Second test should return "2" with Boop!
     else if (count.includes('2')) {
       numberStr = numberStr.concat(", Boop!");
     }
+    //Third test should return "3" with Won't you be my neighbor
     else if (count.includes('1')) {
       numberStr = numberStr.concat(", Beep!");
     }
+    //Fourth test should return the hierarchy of substitutions
     else {
       if (count !== '0') {
         numberStr = numberStr.concat(", ")
